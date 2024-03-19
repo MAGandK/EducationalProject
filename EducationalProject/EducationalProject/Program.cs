c1cdc673b540d841fd9bdb795877e94fd76213dd
AnnaMigitenko <migitenko@gmail.com>
2024-03-19 12:57:45 +0300

    created project

---
+++ EducationalProject/EducationalProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EducationalProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
