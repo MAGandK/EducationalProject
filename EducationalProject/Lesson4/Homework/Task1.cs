0b9f4bc67308e82f0abf8b56a760323683b1cc91
AnnaMigitenko <migitenko@gmail.com>
2024-03-27 17:58:22 +0300

    create lesson4, add prectice, add Task1, add Task2

---
+++ EducationalProject/Lesson4/Homework/Task1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Lesson4.Homework
+{
+    public static class CreateMas
+    {
+        public static void Create()
+        {
+            int[] array = new int[5];
+
+            foreach (var itemArray in array)
+            {
+                Console.WriteLine(itemArray);
+            }
+        }
+    }
+}
---
+++ EducationalProject/Lesson4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Lesson4.Homework;
+
+namespace Lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Practice.Array();
+            Console.WriteLine("------------");
+
+            CreateMas.Create();
+            Console.WriteLine("------------");
+
+            LoopWithCounter.GetNumbers();
+        }
+    }
+}
