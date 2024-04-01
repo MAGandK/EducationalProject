3c90d44bc7bb3b52f3da228424546e78185a717e
AnnaMigitenko <migitenko@gmail.com>
2024-04-01 16:02:32 +0300

    add homework №5

---
+++ EducationalProject/Lesson5/Practice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Lesson5
+{
+    public static class Practice
+    {
+       public static void Display (int r, params int[] numbers)
+        {
+            int result = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                result += numbers[i];
+            }
+
+            result *= r;
+
+            Console.WriteLine(result);
+        }
+    }
+}
