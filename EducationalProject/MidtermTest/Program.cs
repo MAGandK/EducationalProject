38bde9232096e10356d7646f136a9b7aa79cdba4
AnnaMigitenko <migitenko@gmail.com>
2024-05-02 17:51:03 +0300

    add task number 7 from the test

--- EducationalProject/MidtermTest/Program.cs
+++ EducationalProject/MidtermTest/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Text;
+﻿using MidtermTest.Task7;
 
 namespace MidtermTest;
 
@@ -6,16 +6,22 @@ class Program
 {
     static void Main(string[] args)
     {
-       TaskArray.AddChar();
+       //TaskArray.AddChar();
 
-       StringBuilder sb = new StringBuilder("Hello world!");
-       Console.WriteLine("Original string: " + sb);
+       //StringBuilder sb = new StringBuilder("Hello world!");
+       //Console.WriteLine("Original string: " + sb);
 
-       sb.ToUpperString();
-       Console.WriteLine("Modified string: " + sb);
+       //sb.ToUpperString();
+       //Console.WriteLine("Modified string: " + sb);
 
-        MyString str1 = new MyString();
+       // MyString str1 = new MyString();
 
-        str1.Reverse();
+       // str1.Reverse();
+
+        CarBace car1 = new CarFord("Ford", 1, "blak", false);
+        CarBace car2 = new CarAudi("Audi", 2, 4);
+
+        car1.ShowInfo();
+        car2.ShowInfo();
     }
 }
