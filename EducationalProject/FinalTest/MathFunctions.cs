9d649ff8d24060d02c222f9e5de55ff6d531041f
AnnaMigitenko <migitenko@gmail.com>
2024-06-04 21:13:23 +0300

    add MathFunctions

---
+++ EducationalProject/FinalTest/MathFunctions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace FinalTest
+{
+	public class MathFunctions
+	{
+        public static long Factorial(int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentException("Факториал определен только для неотрицательных чисел.");
+            }
+
+            long result = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+        public static double Power(double x, double y)
+        {
+            return Math.Pow(x, y);
+        }
+
+        public static double Square(double x)
+        {
+            if (x < 0)
+            {
+                throw new ArgumentException("Квадрат числа определен только для неотрицательных чисел.");
+            }
+
+            return Math.Sqrt(x);
+        }
+    }
+}
+
+
--- EducationalProject/FinalTest/Program.cs
+++ EducationalProject/FinalTest/Program.cs
@@ -13,6 +13,31 @@ class Program
         Console.WriteLine(result);
 
         Elements.GetElement();
+
+        try
+        {
+            int n = 5;
+            long factorial = MathFunctions.Factorial(n);
+            Console.WriteLine($"Факториал числа {n} равен {factorial}");
+
+            double x = 3;
+            double y = 2;
+            double power = MathFunctions.Power(x, y);
+            Console.WriteLine($"{x} в степени {y} равно {power}");
+
+            double number = 9;
+            double squareRoot = MathFunctions.Square(number);
+            Console.WriteLine($"Корень из {number} равен {squareRoot}");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Исключение: {ex.Message}");
+        }
     }
 }
+
 
