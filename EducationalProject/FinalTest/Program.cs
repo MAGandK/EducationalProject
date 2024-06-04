7d7ce1170c5abae47598311b349d5db8808146a6
AnnaMigitenko <migitenko@gmail.com>
2024-06-04 20:57:29 +0300

    add second

--- EducationalProject/FinalTest/Program.cs
+++ EducationalProject/FinalTest/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace FinalTest;
+﻿using System.Text;
+
+namespace FinalTest;
 
 class Program
 {
     static void Main(string[] args)
     {
         Array.FillArray();
+
+        StringBuilder sb = new StringBuilder("How are you doing?!");
+        string result = StringReturn.ReplaceVowels(sb);
+        Console.WriteLine(result);  
     }
 }
 
---
+++ EducationalProject/FinalTest/StringReturn.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace FinalTest
+{
+    public static class StringReturn
+    {
+        public static string ReplaceVowels(this StringBuilder input)
+        {
+            string vowels = "aeiouAEIOU";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (vowels.Contains(input[i].ToString()))
+                {
+                    input[i] = '*';
+                }
+            }
+
+            return input.ToString();
+        }
+    }
+}
+
+
+
